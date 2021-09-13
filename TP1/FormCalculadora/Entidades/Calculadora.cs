b57759824acfe476f7739f5f56d1b369e95271a3
Fede Gomez <fedegomez.pg@gmail.com>
2021-09-13 18:44:13 -0300

    Form

---
+++ TP1/FormCalculadora/Entidades/Calculadora.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        private static char ValidarOperador(char operador)
+        {
+            char aux;
+            switch (operador)
+            {
+                case '+':
+                case '*':
+                case '/':
+                case '-':
+                    aux = operador;
+                    break;
+
+                default:
+                    aux = '+';
+                    break;
+            }
+
+            return aux;
+            if(operador.ToString() != "-" || operador.ToString() != "+" || 
+                operador.ToString() != "/" || operador.ToString() != "*")
+            {
+                return '+';
+            }
+
+            return operador;
+        }
+
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            double aux;
+            switch (operador)
+            {
+                case '+':
+                    aux = num1 + num2;
+                    break;
+                case '*':
+                    aux = num1 *num2;
+                    break;
+                case '/':
+                    if(ValidarOperando(num2) == 0)
+                    {
+                        aux = num2;
+                    }
+                    else
+                    {
+                        aux = num1 / num2;
+                    }
+                    
+                        break;
+                case '-':
+                    aux = operador;
+                    break;
+
+                default:
+                    aux = '+';
+                    break;
+            }
+            return aux;
+        }
+
+    }
+
+}
---
+++ TP1/FormCalculadora/Entidades/Operando.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public Operando()
+        {
+            this.numero = 0;
+        }
+
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+        public Operando(double numero):this(numero.ToString())
+        {
+        }
+
+        private double ValidarOperando(string strNumero)
+        {
+            double aux;
+            if (double.TryParse(strNumero, out aux))
+            { }
+            else
+            {
+                aux = 0;
+            }
+
+            return aux;
+        }
+        private string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        private bool EsBinario(string binario)
+        {
+            int aux =0;
+            bool rtn;
+            foreach (char i in binario)
+            {
+                switch (binario)
+                {
+                    case "1":
+                    case "0":
+                        break;
+                    default:
+                        aux++;
+                        break;
+                }
+
+            }
+            if (aux >0)
+            {
+                rtn = false;
+            }
+            else
+            {
+                rtn = true;
+            }
+            return rtn;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            int aux =0;
+            if (EsBinario(binario))
+            {
+                foreach (char valor in binario)
+                {
+                    int auxIndice = binario.Length;
+                    if (valor == 1)
+                    {
+                        aux = aux + (valor * (int)Math.Pow(2, auxIndice));
+                    }
+                    auxIndice--;
+                }
+            }
+            return aux.ToString();
+        }
+
+        public string BinarioDecimal(double numero)
+        {
+            int aux = 0;
+
+            while(numero > 1)
+            {
+                if (numero % 2 == 0 || numero % 2 == 1)
+                {
+                    aux = aux + ((int)numero % 2);
+                    numero = numero / 2;
+                }
+            }
+        }
+        public string BinarioDecimal(string numero)
+        {
+
+        }
+    }
+}
